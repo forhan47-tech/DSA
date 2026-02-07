878ee542fc77552d2ff876b6c4d3e65dc0ec6e27
forhan47-tech <forhan.csecu@gmail.com>
2026-02-07 09:37:15 -0600

    Initial commit

---
+++ 02_data_structures/01_arrays/01_1D_arrays/01_basics/02_initialization/dynamic_init.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size: ";
+    cin >> n;
+
+    int* arr = new int[n]; // dynamic allocation
+
+    arr[0] = 10;   // initialization
+    arr[1] = 20;
+
+    // access
+    cout << "arr[0]: " << arr[0] << endl;
+    cout << "arr[1]: " << arr[1] << endl;
+
+    delete[] arr; // free memory
+    return 0;
+}
