ac080395c0aa30079fe304f803df603ee73b4449
forhan47-tech <forhan.csecu@gmail.com>
2026-02-07 09:50:11 -0600

    Initial commit

--- 02_data_structures/01_arrays/01_1D_arrays/01_basics/03_length/1.cpp
+++ 02_data_structures/01_arrays/01_1D_arrays/01_basics/03_length/1.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main() {
     int arr[] = {10, 20, 30, 40, 50};
-    size_t n = sizeof(arr) / sizeof(arr[0]);
+    size_t n = sizeof(arr) / sizeof(arr[0]); // size
     cout << "Array length: " << n << endl;
 }
--- 02_data_structures/01_arrays/01_1D_arrays/01_basics/03_length/2.cpp
+++ 02_data_structures/01_arrays/01_1D_arrays/01_basics/03_length/2.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main() {
     int arr[] = {10, 20, 30, 40, 50};
-    size_t n = size(arr);
+    size_t n = size(arr); // size
     cout << "Array length: " << n << endl;
 }
