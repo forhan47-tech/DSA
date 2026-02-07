214d5fa55dad246520b136970d5e141c85b45df8
forhan47-tech <forhan.csecu@gmail.com>
2026-02-07 09:00:41 -0600

    Initial commit

---
+++ 02_data_structures/01_arrays/01_1D_arrays/01_basics/01_declaration/1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5];    // Declaration only
+
+    arr[0] = 10;   // initialization
+    arr[1] = 20;
+
+    cout << "arr[0]: " << arr[0] << endl;  // access
+    cout << "arr[1]: " << arr[1] << endl;
+    return 0;
+}
