cc045ad024e6c476406b36575af56f61f0b6c9c0
forhan47-tech <forhan.csecu@gmail.com>
2026-02-07 12:30:47 -0600

    Initial commit

---
+++ 02_data_structures/01_arrays/02_2D_arrays/01_basics/04_traversal/col_wise.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int mtx[3][4] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12}
+    };
+
+    int r = 3, c = 4;
+
+    cout << "Column-major traversal:" << endl;
+    for(int j=0; j<c; j++) {
+        for(int i=0; i<r; i++) {
+            cout << mtx[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
--- 02_data_structures/01_arrays/02_2D_arrays/01_basics/04_traversal/row_wise.cpp
+++ 02_data_structures/01_arrays/02_2D_arrays/01_basics/04_traversal/row_wise.cpp
@@ -3,10 +3,11 @@ using namespace std;
 
 int main() {
     int mtx[3][4] = { {1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12} };
+    int r = 3, c = 4;
 
     cout << "Row-major traversal:" << endl;
-    for(int i=0; i<3; i++) {
-        for(int j=0; j<4; j++) {
+    for(int i=0; i<r; i++) {
+        for(int j=0; j<c; j++) {
             cout << mtx[i][j] << " ";
         }
         cout << endl;
