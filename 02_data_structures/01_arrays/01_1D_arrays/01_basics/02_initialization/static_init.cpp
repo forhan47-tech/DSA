9c105af3791552f722cdaf9e7383737174696abe
forhan47-tech <forhan.csecu@gmail.com>
2026-02-07 09:43:48 -0600

    Initial commit

---
+++ 02_data_structures/01_arrays/01_1D_arrays/01_basics/02_initialization/static_init.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5] = {10, 20, 30, 40};    // Declaration + initialization
+
+    // access
+    cout << "arr[0]: " << arr[0] << endl;
+    cout << "arr[1]: " << arr[1] << endl;
+    return 0;
+}
