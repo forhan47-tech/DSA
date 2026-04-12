03a78a934c074682f47c279a77139fd64e229632
forhan47-tech <forhan.csecu@gmail.com>
2026-04-13 02:07:42 +0600

    Initial commit

--- 02_data_structures/02_strings/02_initialization/copy.cpp
+++ 02_data_structures/02_strings/02_initialization/copy.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-    string s1("World");   // Constructor style
+    string s1("World");  
     string s2(s1);        // Copy constructor
     cout << s1 << " " << s2 << endl;
     return 0;
